42dee3a571185bae857978ea705c0c0355b6f7b9
mirchittagong <mirchittagong2020@gmail.com>
2023-11-18 00:16:21 -0500

    A

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,17 @@
+package com.generic;
+
+public class Login {//  Class Starts Here
+	
+	
+	Login obj1 = new Login ();// Objects
+	Login obj2 = new Login ();// Objects
+	Login obj3 = new Login ();// Objects
+	
+	public static void main(String[] args) {//Method Start  Here
+		
+		System.out.println("Hello World, This is AMIN JAMAL, Starting JAVA Programing for the 1st time :)");
+		
+	}// Method Ends Here
+	
+
+}// Class Ends Here
